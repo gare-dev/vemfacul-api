ce247b73b3910bb5bfc29cf0e362fa339bb6d38b
Guilherme Gare <ggare.contato@gmail.com>
2025-02-13 21:45:16 -0300

    fix: adicionado cors

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require("express");
+const cors = require("cors");
 require("dotenv").config();
 
 const app = express();
 const PORT = 3001;
 
+app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
