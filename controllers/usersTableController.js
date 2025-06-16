a204e4e893535c3a2c8d27d3aa6bc915175e5a3f
anndrem <dealmeida.andre08@gmail.com>
2025-06-15 23:38:40 -0300

    cleanup: selecteCached

--- controllers/usersTableController.js
+++ controllers/usersTableController.js
@@ -106,12 +106,6 @@ const usersTableController = {
                     code: "EMAIL_NOT_FOUND"
                 })
             }
-            // const resetToken = crypto.randomBytes(32).toString('hex');
-            // const { error } = await supabase
-            //     .from('password_resets')
-            //     .insert([
-            //         { email: destinatario, token: resetToken }
-            //     ]);
             sendForgotPasswordEmail(email)
             return res.status(200).json({
                 message: "Email enviado com sucesso!",
