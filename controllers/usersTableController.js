bae4fce365a3a41c689ee8eca85b765cd5f0fec7
Guilherme Gare <ggare.contato@gmail.com>
2025-06-17 01:03:53 -0300

    feat: hotfix

--- controllers/usersTableController.js
+++ controllers/usersTableController.js
@@ -53,7 +53,7 @@ const usersTableController = {
             if (response.rowCount >= 1) {
                 const token = jwt.sign({ email: email, image: response.rows[0].foto, name: response.rows[0].nome, id: response.rows[0].id_user, username: response.rows[0].username }, process.env.SECRET, { expiresIn: 36000, })
                 return res.cookie('auth', token, {
-                    sameSite: "none", domain: "invest-api-rose.vercel.app", maxAge: 3600 * 1000 * 10, secure: true
+                    sameSite: "none", secure: true
                 }).status(200).json({
                     message: "Login realizado com sucesso!",
                     code: "LOGIN_SUCCESS"
