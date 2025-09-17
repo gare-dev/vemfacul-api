15493b1a6b69f43faee69993301b940a3a53499e
Guilherme Gare <ggare.contato@gmail.com>
2025-09-16 23:58:19 -0300

    fix

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,10 @@ app.get("/", (req, res) => {
     res.send("OK")
 })
 
+app.get("/health", (req, res) => {
+    res.send(process.env.RESEND_API_KEY?.slice(0, 10))
+})
+
 app.listen(PORT, () => {
     console.log(`🟢 Server running on port ${PORT}`)
 })
