956bc1cf9c4c89a2a108334b0cfbb97220722d3b
anndrem <dealmeida.andre08@gmail.com>
2025-09-29 19:01:34 -0300

    pull

--- src/repositories/NotificationsRepository.ts
+++ src/repositories/NotificationsRepository.ts
@@ -14,7 +14,8 @@ export class NotificationsRepository {
                         join users_table d on d.id_user = n.id_user
                         join users_table a on a.id_user = n.id_actor
                         join postagens_table p on p.id_postagem = n.id_postagem
-                        where d.id_user = $1 and a.id_user != d.id_user`
+                        where d.id_user = $1 and a.id_user != d.id_user
+                        order by n.created_at desc`
 
     if (mode == "redacao") query = ` select
                         u.username,
