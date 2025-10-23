60c5670d7e420e1af94894efcb40a038460f811c
Guilherme Gare <ggare.contato@gmail.com>
2025-10-23 09:30:38 -0300

    seila

--- src/http/server.ts
+++ src/http/server.ts
@@ -38,7 +38,7 @@ app.use(express.json())
 app.use(cookieParser())
 
 // app.use(requestLogger)
-app.use(rateLimitMiddleware)
+// app.use(rateLimitMiddleware)
 
 app.use(UserRoutes)
 app.use(EventRoutes)
